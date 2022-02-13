c6529da2daa172039514561357aaa324d0a3a417
wudan01 <wudan01@100tal.com>
2022-02-13 12:51:17 +0800

    add

--- webpack/test/模块化引入/5. lazy/src/index.js
+++ webpack/test/模块化引入/5. lazy/src/index.js
@@ -4,7 +4,14 @@
  */
 const load = document.getElementById('load');
 load.addEventListener('click', () => {
-  import(/* webpackChunkName: 'title' */ './title').then((result) => {
-    console.log(result.default);
-  });
+  // import(/* webpackChunkName: 'title' */ './title').then((result) => {
+  //   console.log(result.default);
+  // });
+  console.log('arrow')
+  console.log(this)
+});
+
+
+load.addEventListener('click', function () {
+  console.log(this)
 });
